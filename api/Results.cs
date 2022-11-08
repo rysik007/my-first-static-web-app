62a4f981601ceb548296fc7965ece2996f7d5ef0
Bartosz Barczycki <btba@demant.com>
2022-11-08 17:24:31 +0100

    Test with blobs

--- api/Results.cs
+++ api/Results.cs
@@ -7,22 +7,39 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Azure.Storage.Blobs;
 
 namespace Amp.Test
 {
     public static class Results
     {
         [FunctionName("Results")]
-        public static async Task<string> Run(
+        public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
+            string connectionString = Environment.GetEnvironmentVariable("DefaultEndpointsProtocol=https;AccountName=ohconnectdevstorage;AccountKey=U9ZP7uaNh5nyHaGUa4GsZRBG8rjzbaWvTdtbf9v1Wdu1839JAqg3O6nJIW0mGZT6LOIqrMtwytVO+ASt1DTFuA==;EndpointSuffix=core.windows.net");
 
-            string responseMessage =" sBarte";
+            // Create a BlobServiceClient object 
+            var blobServiceClient = new BlobServiceClient(connectionString);
+            //   var blobServiceClient = new BlobServiceClient(
+            //new Uri("https://<storage-account-name>.blob.core.windows.net"),
+            //new DefaultAzureCredential());
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("mydefaultcontainer");
+            using (var stream = new MemoryStream())
+            {
 
-            return responseMessage;
+                stream.Position = 0;
+                using (var sw = new StreamWriter(stream))
+                {
+                    sw.Write("K");
+                }
+
+                containerClient.UploadBlob("Test", stream);
+            }
+            return new OkObjectResult("ok");
         }
     }
 }
